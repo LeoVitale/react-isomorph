e96a9864bf64b252c72f3a848733ee2518f3f7ca
Leonardo Vitale <leonardo_vitale@hotmail.com>
2015-12-09 12:32:31 -0200

    Created twitter

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'E5aRuzk1C7JzXQasZpibBMqbP',
+    consumer_secret: 'KRgXlyW6mWWqFIOtYGu2QhNDlQukyBoy6kTKKUh81xvAlSvx5i',
+    access_token_key: '19516109-rIsILxJJy9Fap0Y7QKDppDPKOSe3XjGTrNK1Opf2U',
+    access_token_secret: 'ewgBcyxFiCkmbZkoy1fxqSijdIaFnBVCMqShl0apKtk1i'
+  }
+}
